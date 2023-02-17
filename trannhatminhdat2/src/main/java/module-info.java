44f35e4c263b15d1c851641d8530ac21c934b7ec
hgooshvhd123@gmail.com <hgooshvhd123@gmail.com>
2023-02-17 13:34:48 +0700

    tao project2

---
+++ trannhatminhdat2/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module nhom7.trannhatminhdat2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens nhom7.trannhatminhdat2 to javafx.fxml;
+    exports nhom7.trannhatminhdat2;
+}
---
+++ trannhatminhdat2/src/main/java/nhom7/trannhatminhdat2/PrimaryController.java
@@ -0,0 +1,12 @@
+package nhom7.trannhatminhdat2;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
